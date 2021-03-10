5f8d7de75a526e01ab7779ec3b8953090b73140b
David Kasali <david.kasali@civica.co.uk>
2021-03-10 09:35:23 +0000

    error prefix added

--- components/input-field/src/index.js
+++ components/input-field/src/index.js
@@ -8,6 +8,7 @@ import LabelText from '@govuk-react/label-text';
 import ErrorText from '@govuk-react/error-text';
 import HintText from '@govuk-react/hint-text';
 import Input from '@govuk-react/input';
+import VisuallyHidden from '@govuk-react/visually-hidden';
 
 /**
  *
@@ -55,11 +56,11 @@ import Input from '@govuk-react/input';
  * - https://github.com/alphagov/govuk-frontend/blob/master/src/components/input/_input.scss
  * - https://github.com/alphagov/govuk_elements/blob/master/assets/sass/elements/_forms.scss
  */
-const InputField = ({ meta, children, hint, input, ...props }) => (
+const InputField = ({ meta, children, hint, input, language, ...props }) => (
   <Label {...props} error={meta.touched && meta.error}>
     <LabelText>{children}</LabelText>
     {hint && <HintText>{hint}</HintText>}
-    {meta.touched && meta.error && <ErrorText>{meta.error}</ErrorText>}
+    {meta.touched && meta.error && <ErrorText><VisuallyHidden> {language === 'cy'? 'Gwall: ' : 'Error: '}</VisuallyHidden>{meta.error}</ErrorText>}
     <Input error={meta.touched && meta.error} {...input} />
   </Label>
 );
