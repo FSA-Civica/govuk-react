036f1854f93d933440e85207b3c0dfb7192a26b9
David Kasali <david.kasali@civica.co.uk>
2021-03-10 15:21:07 +0000

    fixes

--- components/multi-choice/src/index.js
+++ components/multi-choice/src/index.js
@@ -65,7 +65,7 @@ const StyledFieldset = styled('div')(
  * - https://govuk-elements.herokuapp.com/errors/example-form-validation-single-question-radio
  *
  */
-const MultiChoice = ({ meta, label, children, hint, language, ...props }) => (
+const MultiChoice = ({ meta, label, children, hint, ...props }) => (
   <StyledFieldset error={meta.touched && meta.error} {...props}>
     <LabelText>{label}</LabelText>
     {hint && <HintText>{hint}</HintText>}
